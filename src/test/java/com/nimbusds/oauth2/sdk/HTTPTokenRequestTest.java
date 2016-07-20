c39a04a63a5689cf9737208f964dcbb0bc9ab191
Connect2id Support <support@connect2id.com>
2016-07-20 10:01:09 +0300

    Adds HTTP TokenRequest test (iss #192)

---
+++ src/test/java/com/nimbusds/oauth2/sdk/HTTPTokenRequestTest.java
@@ -0,0 +1,90 @@
+package com.nimbusds.oauth2.sdk;
+
+
+import java.net.URI;
+import java.util.Map;
+
+import static net.jadler.Jadler.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.http.CommonContentTypes;
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
+import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
+import com.nimbusds.oauth2.sdk.token.Tokens;
+import com.nimbusds.oauth2.sdk.util.URLUtils;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class HTTPTokenRequestTest {
+	
+	
+	@Before
+	public void setUp() {
+		initJadler();
+	}
+	
+	
+	@After
+	public void tearDown() {
+		closeJadler();
+	}
+	
+	
+	@Test
+	public void testPOST()
+		throws Exception {
+		
+		final AuthorizationCodeGrant codeGrant = new AuthorizationCodeGrant(new AuthorizationCode("abc"), URI.create("https://example.com/cb"));
+		TokenRequest tokenRequest = new TokenRequest(URI.create("http://localhost:" + port() + "/token"), new ClientSecretBasic(new ClientID("123"), new Secret("secret")), codeGrant);
+		
+		BearerAccessToken token = new BearerAccessToken("xyz");
+		AccessTokenResponse tokenResponse = new AccessTokenResponse(new Tokens(token, null));
+		
+		onRequest()
+			.havingMethodEqualTo("POST")
+			.havingPathEqualTo("/token")
+			.havingHeaderEqualTo("Content-Type", CommonContentTypes.APPLICATION_URLENCODED.toString())
+			.havingBody(new BaseMatcher<String>() {
+				@Override
+				public boolean matches(Object o) {
+					Map<String,String> postParams = URLUtils.parseParameters(o.toString());
+					Map<String,String> expectedPostParams = codeGrant.toParameters();
+					for (Map.Entry<String,String> exp: expectedPostParams.entrySet()) {
+						if (! postParams.get(exp.getKey()).equals(exp.getValue()))
+							return false;
+					}
+					return expectedPostParams.size() == postParams.size();
+				}
+				@Override
+				public void describeTo(Description description) {
+					
+				}
+			})
+			.respond()
+			.withStatus(200)
+			.withHeader("Content-Type", "application/json")
+			.withBody(tokenResponse.toJSONObject().toJSONString());
+		
+		HTTPRequest httpRequest = tokenRequest.toHTTPRequest();
+		
+		assertEquals(HTTPRequest.Method.POST, httpRequest.getMethod());
+		assertEquals(CommonContentTypes.APPLICATION_URLENCODED.toString(), httpRequest.getContentType().toString());
+		
+		HTTPResponse httpResponse = httpRequest.send();
+		
+		assertEquals(200, httpResponse.getStatusCode());
+		
+		AccessTokenResponse receivedTokenResponse = AccessTokenResponse.parse(httpResponse);
+		assertEquals(tokenResponse.getTokens().getBearerAccessToken(), receivedTokenResponse.getTokens().getBearerAccessToken());
+		assertNull(receivedTokenResponse.getTokens().getRefreshToken());
+	}
+}
