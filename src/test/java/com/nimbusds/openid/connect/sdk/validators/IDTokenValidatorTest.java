fdedfade2cfb0886904b15ca20bac62a26202d8f
Connect2id Support <support@connect2id.com>
2016-12-12 20:49:48 +0200

    Extends IDTokenValidator test - client_secret too short for HMAC

--- src/test/java/com/nimbusds/openid/connect/sdk/validators/IDTokenValidatorTest.java
+++ src/test/java/com/nimbusds/openid/connect/sdk/validators/IDTokenValidatorTest.java
@@ -361,6 +361,38 @@ public class IDTokenValidatorTest extends TestCase {
 			assertEquals("Signed JWT rejected: Invalid signature", e.getMessage());
 		}
 	}
+
+
+	public void testVerify_secretTooShort()
+		throws Exception {
+
+		Secret clientSecret = new Secret(ByteUtils.byteLength(256));
+
+		Issuer iss = new Issuer("https://c2id.com");
+		ClientID clientID = new ClientID("123");
+		Date now = new Date();
+
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+				.issuer(iss.getValue())
+				.subject("alice")
+				.audience(clientID.getValue())
+				.expirationTime(new Date(now.getTime() + 10*60*1000L))
+				.issueTime(now)
+				.build();
+
+		SignedJWT idToken = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+		idToken.sign(new MACSigner(clientSecret.getValueBytes()));
+
+		// Too short secret
+		IDTokenValidator idTokenValidator = new IDTokenValidator(iss, clientID, JWSAlgorithm.HS256, new Secret(16));
+
+		try {
+			idTokenValidator.validate(idToken, null);
+			fail();
+		} catch (KeyLengthException e) {
+			assertEquals("The secret length must be at least 256 bits", e.getMessage());
+		}
+	}
 	
 	
 	public void testVerifyRS256WhenHmacIsExpected()
