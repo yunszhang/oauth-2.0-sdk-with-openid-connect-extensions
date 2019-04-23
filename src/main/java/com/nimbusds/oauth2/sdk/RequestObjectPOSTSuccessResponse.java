a047cf4d01d64a76a36d46136cf4b41165ca44c3
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2019-04-23 12:25:28 +0300

    Adds RequestObjectPOSTSuccessResponse

---
+++ src/main/java/com/nimbusds/oauth2/sdk/RequestObjectPOSTSuccessResponse.java
@@ -0,0 +1,240 @@
+/*
+ * oauth2-oidc-sdk
+ *
+ * Copyright 2012-2016, Connect2id Ltd and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.nimbusds.oauth2.sdk;
+
+
+import java.net.URI;
+import java.util.Date;
+
+import net.jcip.annotations.Immutable;
+import net.minidev.json.JSONObject;
+
+import com.nimbusds.jwt.util.DateUtils;
+import com.nimbusds.oauth2.sdk.http.CommonContentTypes;
+import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import com.nimbusds.oauth2.sdk.id.Audience;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
+
+
+/**
+ * Request object POST success response.
+ *
+ * <p>Example request object POST success response:
+ *
+ * <pre>
+ * HTTP/1.1 201 Created
+ * Date: Tue, 2 May 2017 15:22:31 GMT
+ * Content-Type: application/json
+ *
+ * {
+ *   "iss"         : "https://c2id.com",
+ *   "aud"         : "s6bhdrkqt3",
+ *   "request_uri" : "urn:requests:aashoo1Ooj6ahc5C",
+ *   "exp"         : 1493738581
+ * }
+ * </pre>
+ *
+ * <p>Related specifications:
+ *
+ * <ul>
+ *     <li>Financial-grade API - Part 2: Read and Write API Security Profile,
+ *         section 7.
+ *     <li>The OAuth 2.0 Authorization Framework: JWT Secured Authorization
+ *         Request (JAR) (draft-ietf-oauth-jwsreq-17).
+ * </ul>
+ */
+@Immutable
+public final class RequestObjectPOSTSuccessResponse implements SuccessResponse {
+	
+	
+	/**
+	 * The issuer.
+	 */
+	private final Issuer iss;
+	
+	
+	/**
+	 * The audience (client ID).
+	 */
+	private final Audience aud;
+	
+	
+	/**
+	 * The request URI.
+	 */
+	private final URI requestURI;
+	
+	
+	/**
+	 * The request URI expiration time.
+	 */
+	private final Date exp;
+	
+	
+	/**
+	 * Creates a new request object POST success response.
+	 *
+	 * @param iss        The issuer. Must not be {@code null}.
+	 * @param aud        The audience (the intended client IDMust not be
+	 *                   {@code null}.).
+	 * @param requestURI The request URI. Must not be {@code null}.
+	 * @param exp        The request URI expiration time. Must not be
+	 *                   {@code null}.
+	 */
+	public RequestObjectPOSTSuccessResponse(final Issuer iss,
+						final Audience aud,
+						final URI requestURI,
+						final Date exp) {
+		if (iss == null) {
+			throw new IllegalArgumentException("The issuer must not be null");
+		}
+		this.iss = iss;
+		
+		if (aud == null) {
+			throw new IllegalArgumentException("The audience must not be null");
+		}
+		this.aud = aud;
+		
+		if (requestURI == null) {
+			throw new IllegalArgumentException("The request URI must not be null");
+		}
+		this.requestURI = requestURI;
+		
+		if (exp == null) {
+			throw new IllegalArgumentException("The request URI expiration time must not be null");
+		}
+		this.exp = exp;
+	}
+	
+	
+	/**
+	 * Returns the issuer.
+	 *
+	 * @return The issuer.
+	 */
+	public Issuer getIssuer() {
+		return iss;
+	}
+	
+	
+	/**
+	 * Returns the audience (the intended client ID).
+	 *
+	 * @return The audience.
+	 */
+	public Audience getAudience() {
+		return aud;
+	}
+	
+	
+	/**
+	 * Returns the request URI.
+	 *
+	 * @return The request URI.
+	 */
+	public URI getRequestURI() {
+		return requestURI;
+	}
+	
+	
+	/**
+	 * Returns the expiration time.
+	 *
+	 * @return The expiration time.
+	 */
+	public Date getExpirationTime() {
+		return exp;
+	}
+	
+	
+	@Override
+	public boolean indicatesSuccess() {
+		return true;
+	}
+	
+	
+	/**
+	 * Returns a JSON object representation of this request object POST
+	 * success response.
+	 *
+	 * @return The JSON object.
+	 */
+	public JSONObject toJSONObject() {
+		
+		JSONObject jsonObject = new JSONObject();
+		
+		jsonObject.put("iss", iss.getValue());
+		jsonObject.put("aud", aud.getValue());
+		jsonObject.put("request_uri", requestURI.toString());
+		jsonObject.put("exp", DateUtils.toSecondsSinceEpoch(exp));
+		
+		return jsonObject;
+	}
+	
+	
+	@Override
+	public HTTPResponse toHTTPResponse() {
+		
+		HTTPResponse httpResponse = new HTTPResponse(HTTPResponse.SC_CREATED);
+		httpResponse.setContentType(CommonContentTypes.APPLICATION_JSON);
+		httpResponse.setContent(toJSONObject().toJSONString());
+		return httpResponse;
+	}
+	
+	
+	/**
+	 * Parses a request object POST success response from the specified
+	 * JSON object.
+	 *
+	 * @param jsonObject The JSON object to parse. Must not be {@code null}.
+	 *
+	 * @return The request object POST success response.
+	 *
+	 * @throws ParseException If the JSON object couldn't be parsed to a
+	 *                        request object POST success response.
+	 */
+	public static RequestObjectPOSTSuccessResponse parse(final JSONObject jsonObject)
+		throws ParseException {
+		
+		return new RequestObjectPOSTSuccessResponse(
+			new Issuer(JSONObjectUtils.getString(jsonObject, "iss")),
+			new Audience(JSONObjectUtils.getString(jsonObject, "aud")),
+			JSONObjectUtils.getURI(jsonObject, "request_uri"),
+			DateUtils.fromSecondsSinceEpoch(JSONObjectUtils.getLong(jsonObject, "exp")));
+	}
+	
+	
+	/**
+	 * Parses an request object POST success response from the specified
+	 * HTTP response.
+	 *
+	 * @param httpResponse The HTTP response. Must not be {@code null}.
+	 *
+	 * @return The request object POST success response.
+	 *
+	 * @throws ParseException If the HTTP response couldn't be parsed to a
+	 *                        request object POST success response.
+	 */
+	public static RequestObjectPOSTSuccessResponse parse(final HTTPResponse httpResponse)
+		throws ParseException {
+		
+		httpResponse.ensureStatusCode(HTTPResponse.SC_CREATED, HTTPResponse.SC_OK);
+		return parse(httpResponse.getContentAsJSONObject());
+	}
+}
---
+++ src/test/java/com/nimbusds/oauth2/sdk/RequestObjectPOSTSuccessResponseTest.java
@@ -0,0 +1,249 @@
+/*
+ * oauth2-oidc-sdk
+ *
+ * Copyright 2012-2016, Connect2id Ltd and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.nimbusds.oauth2.sdk;
+
+
+import java.net.URI;
+import java.util.Date;
+
+import junit.framework.TestCase;
+import net.minidev.json.JSONObject;
+
+import com.nimbusds.jwt.util.DateUtils;
+import com.nimbusds.oauth2.sdk.http.CommonContentTypes;
+import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import com.nimbusds.oauth2.sdk.id.Audience;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+
+
+public class RequestObjectPOSTSuccessResponseTest extends TestCase {
+
+	
+	public void testLifeCycle() throws ParseException {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		RequestObjectPOSTSuccessResponse response = new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, exp);
+		
+		assertEquals(issuer, response.getIssuer());
+		assertEquals(audience, response.getAudience());
+		assertEquals(requestURI, response.getRequestURI());
+		assertEquals(exp, response.getExpirationTime());
+		
+		assertTrue(response.indicatesSuccess());
+		
+		JSONObject jsonObject = response.toJSONObject();
+		assertEquals(issuer.getValue(), jsonObject.get("iss"));
+		assertEquals(audience.getValue(), jsonObject.get("aud"));
+		assertEquals(requestURI.toString(), jsonObject.get("request_uri"));
+		assertEquals(expTs, jsonObject.get("exp"));
+		assertEquals(4, jsonObject.size());
+		
+		HTTPResponse httpResponse = response.toHTTPResponse();
+		assertEquals(201, httpResponse.getStatusCode());
+		assertEquals(CommonContentTypes.APPLICATION_JSON.toString(), httpResponse.getContentType().toString());
+		
+		jsonObject = httpResponse.getContentAsJSONObject();
+		assertEquals(issuer.getValue(), jsonObject.get("iss"));
+		assertEquals(audience.getValue(), jsonObject.get("aud"));
+		assertEquals(requestURI.toString(), jsonObject.get("request_uri"));
+		assertEquals(expTs, jsonObject.get("exp"));
+		assertEquals(4, jsonObject.size());
+		
+		response = RequestObjectPOSTSuccessResponse.parse(jsonObject);
+		
+		assertEquals(issuer, response.getIssuer());
+		assertEquals(audience, response.getAudience());
+		assertEquals(requestURI, response.getRequestURI());
+		assertEquals(exp, response.getExpirationTime());
+		
+		response = RequestObjectPOSTSuccessResponse.parse(httpResponse);
+		
+		assertEquals(issuer, response.getIssuer());
+		assertEquals(audience, response.getAudience());
+		assertEquals(requestURI, response.getRequestURI());
+		assertEquals(exp, response.getExpirationTime());
+	}
+	
+	
+	public void testRejectNullParams() {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		try {
+			new RequestObjectPOSTSuccessResponse(null, audience, requestURI, exp);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("The issuer must not be null", e.getMessage());
+		}
+		
+		try {
+			new RequestObjectPOSTSuccessResponse(issuer, null, requestURI, exp);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("The audience must not be null", e.getMessage());
+		}
+		
+		try {
+			new RequestObjectPOSTSuccessResponse(issuer, audience, null, exp);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("The request URI must not be null", e.getMessage());
+		}
+		
+		try {
+			new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("The request URI expiration time must not be null", e.getMessage());
+		}
+	}
+	
+	
+	public void testParseJSONObject_missingParams() {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		RequestObjectPOSTSuccessResponse response = new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, exp);
+		
+		JSONObject jsonObject = response.toJSONObject();
+		jsonObject.remove("iss");
+		try {
+			RequestObjectPOSTSuccessResponse.parse(jsonObject);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing JSON object member with key \"iss\"", e.getMessage());
+		}
+		
+		jsonObject = response.toJSONObject();
+		jsonObject.remove("aud");
+		try {
+			RequestObjectPOSTSuccessResponse.parse(jsonObject);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing JSON object member with key \"aud\"", e.getMessage());
+		}
+		
+		jsonObject = response.toJSONObject();
+		jsonObject.remove("request_uri");
+		try {
+			RequestObjectPOSTSuccessResponse.parse(jsonObject);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing JSON object member with key \"request_uri\"", e.getMessage());
+		}
+		
+		jsonObject = response.toJSONObject();
+		jsonObject.remove("exp");
+		try {
+			RequestObjectPOSTSuccessResponse.parse(jsonObject);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing JSON object member with key \"exp\"", e.getMessage());
+		}
+	}
+	
+	
+	public void testParseHTTPResponse_unexpectedStatusCode() {
+		
+		try {
+			RequestObjectPOSTSuccessResponse.parse(new HTTPResponse(HTTPResponse.SC_UNAUTHORIZED));
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Unexpected HTTP status code 401, must be [201, 200]", e.getMessage());
+		}
+	}
+	
+	
+	public void testParseMissingContentTypeHeader() throws ParseException {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		RequestObjectPOSTSuccessResponse response = new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, exp);
+		HTTPResponse httpResponse = response.toHTTPResponse();
+		httpResponse.setContentType((String)null);
+		
+		try {
+			RequestObjectPOSTSuccessResponse.parse(httpResponse);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing HTTP Content-Type header", e.getMessage());
+		}
+	}
+	
+	
+	public void testParseInvalidJSON() {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		RequestObjectPOSTSuccessResponse response = new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, exp);
+		HTTPResponse httpResponse = response.toHTTPResponse();
+		httpResponse.setContent("text plain");
+		
+		try {
+			RequestObjectPOSTSuccessResponse.parse(httpResponse);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Invalid JSON: Unexpected token text plain at position 10.", e.getMessage());
+		}
+	}
+	
+	
+	public void testParseMissingIssuer() {
+		
+		Issuer issuer = new Issuer("https://c2id.com");
+		Audience audience = new Audience("123");
+		URI requestURI = URI.create("urn:requests:aashoo1Ooj6ahc5C");
+		long expTs = DateUtils.toSecondsSinceEpoch(new Date());
+		Date exp = DateUtils.fromSecondsSinceEpoch(expTs);
+		
+		RequestObjectPOSTSuccessResponse response = new RequestObjectPOSTSuccessResponse(issuer, audience, requestURI, exp);
+		HTTPResponse httpResponse = response.toHTTPResponse();
+		
+		JSONObject jsonObject = response.toJSONObject();
+		jsonObject.remove("iss");
+		httpResponse.setContent(jsonObject.toJSONString());
+		
+		try {
+			RequestObjectPOSTSuccessResponse.parse(httpResponse);
+			fail();
+		} catch (ParseException e) {
+			assertEquals("Missing JSON object member with key \"iss\"", e.getMessage());
+		}
+	}
+}
