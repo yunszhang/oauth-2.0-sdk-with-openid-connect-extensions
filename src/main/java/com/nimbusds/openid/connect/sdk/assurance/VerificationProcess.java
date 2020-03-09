8457da257b6bda171161bf70f885d909fbf5aeae
Yavor <yv@connect2id.com>
2020-03-09 13:43:11 +0200

    New VerificationProcess class

---
+++ src/main/java/com/nimbusds/openid/connect/sdk/assurance/VerificationProcess.java
@@ -0,0 +1,55 @@
+/*
+ * oauth2-oidc-sdk
+ *
+ * Copyright 2012-2016, Connect2id Ltd and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.nimbusds.openid.connect.sdk.assurance;
+
+
+import net.jcip.annotations.Immutable;
+
+import com.nimbusds.oauth2.sdk.id.Identifier;
+
+
+/**
+ * Verification process identifier.
+ *
+ * <p>Related specifications:
+ *
+ * <ul>
+ *     <li>OpenID Connect for Identity Assurance 1.0, section 4.1.
+ * </ul>
+ */
+@Immutable
+public final class VerificationProcess extends Identifier {
+	
+	/**
+	 * Creates a new verification process identifier.
+	 *
+	 * @param value The verification process identifier value. Must not be
+	 *              {@code null}.
+	 */
+	public VerificationProcess(final String value) {
+		super(value);
+	}
+	
+	
+	@Override
+	public boolean equals(final Object object) {
+		
+		return object instanceof VerificationProcess &&
+			this.toString().equals(object.toString());
+	}
+}
