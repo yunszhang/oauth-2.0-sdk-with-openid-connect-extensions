a8b960c9c6f8b91e70d07ac3fb21b0ba85df1242
Yavor <yv@connect2id.com>
2020-04-01 20:09:48 +0300

    Extracts PolicyOperationFactory interface (iss #283)

--- src/main/java/com/nimbusds/openid/connect/sdk/federation/policy/operations/PolicyOperationFactory.java
+++ src/main/java/com/nimbusds/openid/connect/sdk/federation/policy/operations/PolicyOperationFactory.java
@@ -1,7 +1,7 @@
 /*
  * oauth2-oidc-sdk
  *
- * Copyright 2012-2016, Connect2id Ltd and contributors.
+ * Copyright 2012-2020, Connect2id Ltd and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
  * this file except in compliance with the License. You may obtain a copy of the
@@ -23,22 +23,10 @@ import com.nimbusds.openid.connect.sdk.federation.policy.language.PolicyOperatio
 
 
 /**
- * The default policy operation factory.
+ * Policy operation factory.
  *
- * <p>Supports all standard OpenID Connect federation policy operations:
- *
- * <ul>
- *     <li>{@link SubsetOfOperation subset_of}
- *     <li>{@link OneOfOperation one_of}
- *     <li>{@link SupersetOfOperation superset_of}
- *     <li>{@link AddOperation add}
- *     <li>{@link ValueOperation value}
- *     <li>{@link DefaultOperation default}
- *     <li>{@link EssentialOperation essential}
- * </ul>
- *
- * <p>Override the {@link #createForName} method to support additional custom
- * policies.
+ * <p>To support non-standard policy operations on metadata parameters consider
+ * overriding {@link DefaultPolicyOperationFactory#createForName}.
  *
  * <p>Related specifications:
  *
@@ -46,7 +34,7 @@ import com.nimbusds.openid.connect.sdk.federation.policy.language.PolicyOperatio
  *     <li>OpenID Connect Federation 1.0, section 4.1.
  * </ul>
  */
-public class PolicyOperationFactory {
+public interface PolicyOperationFactory {
 	
 	
 	/**
@@ -56,24 +44,5 @@ public class PolicyOperationFactory {
 	 *
 	 * @return The policy operation, {@code null} if not supported.
 	 */
-	public PolicyOperation createForName(final OperationName name) {
-		
-		if (SubsetOfOperation.NAME.equals(name)) {
-			return new SubsetOfOperation();
-		} else if (OneOfOperation.NAME.equals(name)) {
-			return new OneOfOperation();
-		} else if (SupersetOfOperation.NAME.equals(name)) {
-			return new SupersetOfOperation();
-		} else if (AddOperation.NAME.equals(name)) {
-			return new AddOperation();
-		} else if (ValueOperation.NAME.equals(name)) {
-			return new ValueOperation();
-		} else if (DefaultOperation.NAME.equals(name)) {
-			return new DefaultOperation();
-		} else if (EssentialOperation.NAME.equals(name)) {
-			return new EssentialOperation();
-		} else {
-			return null;
-		}
-	}
+	PolicyOperation createForName(OperationName name);
 }
