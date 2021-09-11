cd09726eef893bfa8f2d8ee412cfb929cdf79b4e
Wei Huang <grubbyfans@gmail.com>
2021-09-11 15:35:53 +0200

    Use URI.create to simplify code

--- src/main/java/com/nimbusds/oauth2/sdk/token/TokenTypeURI.java
+++ src/main/java/com/nimbusds/oauth2/sdk/token/TokenTypeURI.java
@@ -2,6 +2,7 @@ package com.nimbusds.oauth2.sdk.token;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,45 +15,37 @@ public class TokenTypeURI {
    * Indicates that the token is an OAuth 2.0 access token issued by the given
    * authorization server.
    */
-  public static final TokenTypeURI ACCESS_TOKEN;
+  public static final TokenTypeURI ACCESS_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:access_token"));
   /**
    * Indicates that the token is an OAuth 2.0 refresh token issued by the given
    * authorization server.
    */
-  public static final TokenTypeURI REFRESH_TOKEN;
+  public static final TokenTypeURI REFRESH_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:refresh_token"));
   /**
    * Indicates that the token is an ID Token as defined in Section 2 of OpenID.Core.
    */
-  public static final TokenTypeURI ID_TOKEN;
+  public static final TokenTypeURI ID_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:id_token"));
   /**
    *  Indicates that the token is a base64url-encoded SAML 1.1
    *  OASIS.saml-core-1.1 assertion.
    */
-  public static final TokenTypeURI SAML1;
+  public static final TokenTypeURI SAML1 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml1"));
   /**
    *  Indicates that the token is a base64url-encoded SAML 2.0
    *  OASIS.saml-core-2.0-os assertion.
    */
-  public static final TokenTypeURI SAML2;
+  public static final TokenTypeURI SAML2 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml2"));
 
-  private static final Map<String, TokenTypeURI> KNOWN_TOKEN_TYPE_URIS = new HashMap<>();
+  private static final Map<String, TokenTypeURI> KNOWN_TOKEN_TYPE_URIS;
 
   static {
-    try {
-      ACCESS_TOKEN = new TokenTypeURI(new URI("urn:ietf:params:oauth:token-type:access_token"));
-      REFRESH_TOKEN = new TokenTypeURI(new URI("urn:ietf:params:oauth:token-type:refresh_token"));
-      ID_TOKEN = new TokenTypeURI(new URI("urn:ietf:params:oauth:token-type:id_token"));
-      SAML1 = new TokenTypeURI(new URI("urn:ietf:params:oauth:token-type:saml1"));
-      SAML2 = new TokenTypeURI(new URI("urn:ietf:params:oauth:token-type:saml2"));
-
-      KNOWN_TOKEN_TYPE_URIS.put(ACCESS_TOKEN.getUri().toString(), ACCESS_TOKEN);
-      KNOWN_TOKEN_TYPE_URIS.put(REFRESH_TOKEN.getUri().toString(), REFRESH_TOKEN);
-      KNOWN_TOKEN_TYPE_URIS.put(ID_TOKEN.getUri().toString(), ID_TOKEN);
-      KNOWN_TOKEN_TYPE_URIS.put(SAML1.getUri().toString(), SAML1);
-      KNOWN_TOKEN_TYPE_URIS.put(SAML2.getUri().toString(), SAML2);
-    } catch (URISyntaxException e) {
-      throw new RuntimeException(e);
-    }
+    Map<String, TokenTypeURI> knownTokenTypeUris = new HashMap<>();
+    knownTokenTypeUris.put(ACCESS_TOKEN.getUri().toString(), ACCESS_TOKEN);
+    knownTokenTypeUris.put(REFRESH_TOKEN.getUri().toString(), REFRESH_TOKEN);
+    knownTokenTypeUris.put(ID_TOKEN.getUri().toString(), ID_TOKEN);
+    knownTokenTypeUris.put(SAML1.getUri().toString(), SAML1);
+    knownTokenTypeUris.put(SAML2.getUri().toString(), SAML2);
+    KNOWN_TOKEN_TYPE_URIS = Collections.unmodifiableMap(knownTokenTypeUris);
   }
 
   private final URI uri;
