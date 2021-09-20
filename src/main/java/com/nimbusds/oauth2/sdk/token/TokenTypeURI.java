5e6de880af5ee959acfb86f4d7e92ddca193ad11
Yavor Vasilev <yv@connect2id.com>
2021-09-20 15:10:05 +0300

    Makes TokenTypeURI final, updates and extends tests

--- src/main/java/com/nimbusds/oauth2/sdk/token/TokenTypeURI.java
+++ src/main/java/com/nimbusds/oauth2/sdk/token/TokenTypeURI.java
@@ -1,99 +1,136 @@
 package com.nimbusds.oauth2.sdk.token;
 
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.jcip.annotations.Immutable;
+
+
 /**
- * Introduced to represent token types(URI) used in TokenExchangeGrant
+ * Token type URI. A URN used to identify the type of token in a token
+ * exchange. The token type URIs can potentially be used in other contexts.
+ *
+ * <p>The standard OAuth URIs are registered at IANA, see
+ * https://www.iana.org/assignments/oauth-parameters/oauth-parameters.xhtml#uri
+ *
+ * <ul>
+ *     <li>OAuth 2.0 Token Exchange (RFC 8693), section 3.
+ * </ul>
  */
-public class TokenTypeURI {
-
-  /**
-   * Indicates that the token is an OAuth 2.0 access token issued by the given
-   * authorization server.
-   */
-  public static final TokenTypeURI ACCESS_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:access_token"));
-  /**
-   * Indicates that the token is an OAuth 2.0 refresh token issued by the given
-   * authorization server.
-   */
-  public static final TokenTypeURI REFRESH_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:refresh_token"));
-  /**
-   * Indicates that the token is an ID Token as defined in Section 2 of OpenID.Core.
-   */
-  public static final TokenTypeURI ID_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:id_token"));
-  /**
-   *  Indicates that the token is a base64url-encoded SAML 1.1
-   *  OASIS.saml-core-1.1 assertion.
-   */
-  public static final TokenTypeURI SAML1 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml1"));
-  /**
-   *  Indicates that the token is a base64url-encoded SAML 2.0
-   *  OASIS.saml-core-2.0-os assertion.
-   */
-  public static final TokenTypeURI SAML2 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml2"));
-
-  private static final Map<String, TokenTypeURI> KNOWN_TOKEN_TYPE_URIS;
-
-  static {
-    Map<String, TokenTypeURI> knownTokenTypeUris = new HashMap<>();
-    knownTokenTypeUris.put(ACCESS_TOKEN.getURI().toString(), ACCESS_TOKEN);
-    knownTokenTypeUris.put(REFRESH_TOKEN.getURI().toString(), REFRESH_TOKEN);
-    knownTokenTypeUris.put(ID_TOKEN.getURI().toString(), ID_TOKEN);
-    knownTokenTypeUris.put(SAML1.getURI().toString(), SAML1);
-    knownTokenTypeUris.put(SAML2.getURI().toString(), SAML2);
-    KNOWN_TOKEN_TYPE_URIS = Collections.unmodifiableMap(knownTokenTypeUris);
-  }
-
-  private final URI uri;
-
-  private TokenTypeURI(URI uri) {
-    if (uri == null) {
-      throw new IllegalArgumentException("The uri must not be null");
-    }
-
-    this.uri = uri;
-  }
-
-  public URI getURI() {
-    return uri;
-  }
-
-  /**
-   * Parses TokenTypeURI from a string value
-   *
-   * @param uriValue uri in string type
-   * @return Parsed token type uri
-   * @throws URISyntaxException if uriValue is an invalid uri
-   */
-  public static TokenTypeURI parse(String uriValue) throws URISyntaxException {
-    if (uriValue == null) {
-      throw new IllegalArgumentException("The uri value must not be null");
-    }
-
-    if (KNOWN_TOKEN_TYPE_URIS.containsKey(uriValue)) {
-      return KNOWN_TOKEN_TYPE_URIS.get(uriValue);
-    }
-
-    URI uri = new URI(uriValue);
-    return new TokenTypeURI(uri);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    TokenTypeURI that = (TokenTypeURI) o;
-
-    return uri.equals(that.getURI());
-  }
-
-  @Override
-  public int hashCode() {
-    return uri.hashCode();
-  }
+@Immutable
+public final class TokenTypeURI {
+	
+	
+	/**
+	 * The token type URI for an OAuth 2.0 access token.
+	 */
+	public static final TokenTypeURI ACCESS_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:access_token"));
+	
+	
+	/**
+	 * The token type URI for an OAuth 2.0 refresh token.
+	 */
+	public static final TokenTypeURI REFRESH_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:refresh_token"));
+	
+	
+	/**
+	 * The token type URI for an OpenID Connect ID Token.
+	 */
+	public static final TokenTypeURI ID_TOKEN = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:id_token"));
+	
+	
+	/**
+	 * The token type URI for a BASE64URL-encoded SAML 1.1 assertion.
+	 */
+	public static final TokenTypeURI SAML1 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml1"));
+	
+	
+	/**
+	 * The token type URI for a BASE64URL-encoded SAML 2.0 assertion.
+	 */
+	public static final TokenTypeURI SAML2 = new TokenTypeURI(URI.create("urn:ietf:params:oauth:token-type:saml2"));
+	
+	
+	private static final Map<String, TokenTypeURI> KNOWN_TOKEN_TYPE_URIS;
+	
+	static {
+		Map<String, TokenTypeURI> knownTokenTypeUris = new HashMap<>();
+		knownTokenTypeUris.put(ACCESS_TOKEN.getURI().toString(), ACCESS_TOKEN);
+		knownTokenTypeUris.put(REFRESH_TOKEN.getURI().toString(), REFRESH_TOKEN);
+		knownTokenTypeUris.put(ID_TOKEN.getURI().toString(), ID_TOKEN);
+		knownTokenTypeUris.put(SAML1.getURI().toString(), SAML1);
+		knownTokenTypeUris.put(SAML2.getURI().toString(), SAML2);
+		KNOWN_TOKEN_TYPE_URIS = Collections.unmodifiableMap(knownTokenTypeUris);
+	}
+	
+	private final URI uri;
+	
+	
+	/**
+	 * Creates a new token type URI with the specified value.
+	 *
+	 * @param uri The URI value. Must not be {@code null}.
+	 */
+	private TokenTypeURI(final URI uri) {
+		if (uri == null) {
+			throw new IllegalArgumentException("The URI must not be null");
+		}
+		this.uri = uri;
+	}
+	
+	
+	/**
+	 * Returns the URI for this token type.
+	 *
+	 * @return The URI.
+	 */
+	public URI getURI() {
+		return uri;
+	}
+	
+	
+	/**
+	 * Parses a token type URI from the specified string.
+	 *
+	 * @param uriValue The URI string value. Must not be {@code null}.
+	 *
+	 * @return The token type URI.
+	 *
+	 * @throws URISyntaxException If the URI value is illegal.
+	 */
+	public static TokenTypeURI parse(final String uriValue)
+		throws URISyntaxException {
+		
+		if (uriValue == null) {
+			throw new IllegalArgumentException("The URI value must not be null");
+		}
+		
+		if (KNOWN_TOKEN_TYPE_URIS.containsKey(uriValue)) {
+			return KNOWN_TOKEN_TYPE_URIS.get(uriValue);
+		}
+		
+		URI uri = new URI(uriValue);
+		return new TokenTypeURI(uri);
+	}
+	
+	
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		TokenTypeURI that = (TokenTypeURI) o;
+		
+		return uri.equals(that.getURI());
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return uri.hashCode();
+	}
 }
--- src/test/java/com/nimbusds/oauth2/sdk/token/TokenTypeURITest.java
+++ src/test/java/com/nimbusds/oauth2/sdk/token/TokenTypeURITest.java
@@ -1,33 +1,50 @@
 package com.nimbusds.oauth2.sdk.token;
 
-import com.nimbusds.oauth2.sdk.ParseException;
+
+import java.net.URI;
 import java.net.URISyntaxException;
+
 import junit.framework.TestCase;
 
+
 public class TokenTypeURITest extends TestCase {
-
-  public void testParseKnownUri() throws URISyntaxException {
-    TokenTypeURI tokenTypeURI1 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:access_token");
-    TokenTypeURI tokenTypeURI2 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:access_token");
-
-    assertSame(tokenTypeURI1, tokenTypeURI2);
-  }
-
-  public void testParseUnknownUri() throws URISyntaxException {
-    TokenTypeURI tokenTypeURI1 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:unknown_token");
-    TokenTypeURI tokenTypeURI2 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:unknown_token");
-
-    assertEquals(tokenTypeURI1, tokenTypeURI2);
-    assertEquals(tokenTypeURI1.hashCode(), tokenTypeURI2.hashCode());
-  }
-
-  public void testParseNullUri() {
-    try {
-      TokenTypeURI.parse(null);
-      fail();
-    } catch (Exception e) {
-      assertTrue(e instanceof IllegalArgumentException);
-      assertEquals("The uri value must not be null", e.getMessage());
-    }
-  }
+	
+	
+	public void testConstants() {
+		
+		assertEquals(URI.create("urn:ietf:params:oauth:token-type:access_token"), TokenTypeURI.ACCESS_TOKEN.getURI());
+		assertEquals(URI.create("urn:ietf:params:oauth:token-type:refresh_token"), TokenTypeURI.REFRESH_TOKEN.getURI());
+		assertEquals(URI.create("urn:ietf:params:oauth:token-type:id_token"), TokenTypeURI.ID_TOKEN.getURI());
+		assertEquals(URI.create("urn:ietf:params:oauth:token-type:saml1"), TokenTypeURI.SAML1.getURI());
+		assertEquals(URI.create("urn:ietf:params:oauth:token-type:saml2"), TokenTypeURI.SAML2.getURI());
+	}
+	
+	
+	public void testParseKnownUri() throws URISyntaxException {
+		TokenTypeURI tokenTypeURI1 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:access_token");
+		TokenTypeURI tokenTypeURI2 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:access_token");
+		
+		assertEquals(tokenTypeURI1, tokenTypeURI2);
+		assertEquals(tokenTypeURI1.hashCode(), tokenTypeURI2.hashCode());
+	}
+	
+	
+	public void testParseUnknownUri() throws URISyntaxException {
+		TokenTypeURI tokenTypeURI1 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:unknown_token");
+		TokenTypeURI tokenTypeURI2 = TokenTypeURI.parse("urn:ietf:params:oauth:token-type:unknown_token");
+		
+		assertEquals(tokenTypeURI1, tokenTypeURI2);
+		assertEquals(tokenTypeURI1.hashCode(), tokenTypeURI2.hashCode());
+	}
+	
+	
+	public void testParseNullUri() {
+		try {
+			TokenTypeURI.parse(null);
+			fail();
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+			assertEquals("The URI value must not be null", e.getMessage());
+		}
+	}
 }
