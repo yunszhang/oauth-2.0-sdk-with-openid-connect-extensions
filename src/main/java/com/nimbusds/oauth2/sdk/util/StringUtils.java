f542e3189b3ad7a37121b31999f00be8676cdeb0
Yavor Vasilev <yv@connect2id.com>
2021-11-01 14:10:27 +0200

    Adds static StringUtils.isAlpha method

--- src/main/java/com/nimbusds/oauth2/sdk/util/StringUtils.java
+++ src/main/java/com/nimbusds/oauth2/sdk/util/StringUtils.java
@@ -64,6 +64,31 @@ public final class StringUtils {
 	
 	
 	/**
+	 * Returns {@code true} if the specified char sequence is all
+	 * alphabetic letters.
+	 *
+	 * @param cs The char sequence. May be {@code null}.
+	 *
+	 * @return {@code true} if the specified char sequence is all
+	 *         alphabetic letters, empty or {@code null}, else
+	 *         {@code false}.
+	 */
+	public static boolean isAlpha(final CharSequence cs) {
+		
+		int strLen;
+		if (cs != null && (strLen = cs.length()) != 0) {
+			for(int i = 0; i < strLen; ++i) {
+				if (!Character.isAlphabetic(cs.charAt(i))) {
+					return false;
+				}
+			}
+			
+		}
+		return true;
+	}
+	
+	
+	/**
 	 * Prevents public instantiation.
 	 */
 	private StringUtils() {}
---
+++ src/test/java/com/nimbusds/oauth2/sdk/util/StringUtilsTest.java
@@ -0,0 +1,40 @@
+/*
+ * oauth2-oidc-sdk
+ *
+ * Copyright 2012-2021, Connect2id Ltd and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.nimbusds.oauth2.sdk.util;
+
+
+import junit.framework.TestCase;
+
+
+public class StringUtilsTest extends TestCase {
+
+
+	public void testIsAlpha() {
+	
+		String s = "TheQuickBrownFoxJumpsOverTheLazyDog";
+		
+		assertTrue(StringUtils.isAlpha(s));
+		
+		assertTrue(StringUtils.isAlpha(null));
+		assertTrue(StringUtils.isAlpha(""));
+		
+		assertFalse(StringUtils.isAlpha(" "));
+		assertFalse(StringUtils.isAlpha("123"));
+		assertFalse(StringUtils.isAlpha("123abc"));
+	}
+}
